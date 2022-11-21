8f38b8e7674d5f1d380521d49bd51ae35b0e9d36
dave <dave@thecoderscorner.com>
2022-11-21 08:42:59 +0000

    temporary push to get onto another device

--- tcMenuGenerator/src/main/java/com/thecoderscorner/menu/editorui/generator/font/FontEncoder.java
+++ tcMenuGenerator/src/main/java/com/thecoderscorner/menu/editorui/generator/font/FontEncoder.java
@@ -1,11 +1,12 @@
 package com.thecoderscorner.menu.editorui.generator.font;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
 public interface FontEncoder {
     enum FontFormat { ADAFRUIT, TC_UNICODE}
 
-    public void encodeFontToStream(OutputStream stream, FontFormat fmt);
+    public void encodeFontToStream(OutputStream stream, FontFormat fmt) throws IOException;
     public ByteBuffer encodeFontAsByteArray(FontFormat fmt);
 }
--- tcMenuGenerator/src/test/java/com/thecoderscorner/menu/editorui/generator/font/TcUnicodeFontTest.java
+++ tcMenuGenerator/src/test/java/com/thecoderscorner/menu/editorui/generator/font/TcUnicodeFontTest.java
@@ -6,23 +6,31 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-import static com.thecoderscorner.menu.editorui.generator.font.TcUnicodeFont.*;
+import static com.thecoderscorner.menu.editorui.generator.font.TcUnicodeFontExporter.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TcUnicodeFontTest {
-    private List<TcUnicodeFontItem> fontItems;
-    private TcUnicodeFont font;
+    private List<TcUnicodeFontGlyph> fontItems1;
+    private List<TcUnicodeFontGlyph> fontItems2;
+    private List<TcUnicodeFontBlock> fontBlocks;
+    private TcUnicodeFontExporter font;
 
     @BeforeEach
     void setUp() {
-        fontItems = List.of(
-                new TcUnicodeFontItem(100, new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 16, 12, 16, 0, -18),
-                new TcUnicodeFontItem(101, new byte[]{ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, 15, 12, 14, 1, -12),
-                new TcUnicodeFontItem(102, new byte[]{ 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}, 16, 11, 13, 2, -1),
-                new TcUnicodeFontItem(103, new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 10, 11, 16, 0, -10),
-                new TcUnicodeFontItem(104, new byte[]{ 10, 21, 32, 43, 54, 65, 76, 87, 98, 90}, 4, 11, 16, 1, -11)
+        fontItems1 = List.of(
+                new TcUnicodeFontGlyph(100, new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 16, 12, 16, 0, -18),
+                new TcUnicodeFontGlyph(101, new byte[]{ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, 15, 12, 14, 1, -12),
+                new TcUnicodeFontGlyph(102, new byte[]{ 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}, 16, 11, 13, 2, -1)
         );
-        font = new TcUnicodeFont("myFont123", fontItems, 12);
+        fontItems2 = List.of(
+                new TcUnicodeFontGlyph(103, new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 10, 11, 16, 0, -10),
+                new TcUnicodeFontGlyph(104, new byte[]{ 10, 21, 32, 43, 54, 65, 76, 87, 98, 90}, 4, 11, 16, 1, -11)
+        );
+        fontBlocks = List.of(
+                new TcUnicodeFontBlock(UnicodeBlockMapping.BASIC_LATIN, fontItems1),
+                new TcUnicodeFontBlock(UnicodeBlockMapping.LATIN_EXTENDED_G, fontItems2)
+        );
+        font = new TcUnicodeFontExporter("myFont123", fontBlocks, 12);
     }
 
     @Test
