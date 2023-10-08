b9c3df73aae183493b5bcdd18360d6e13d557f68
dave <dave@nutricherry.com>
2023-10-08 08:15:55 +0100

    #408 interim commit, adding final support for mobile form generation.

--- embedCONTROLCore/src/main/java/com/thecoderscorner/embedcontrol/jfx/controlmgr/AnalogMeterComponent.java
+++ embedCONTROLCore/src/main/java/com/thecoderscorner/embedcontrol/jfx/controlmgr/AnalogMeterComponent.java
@@ -94,7 +94,8 @@ public class AnalogMeterComponent<T extends Number> extends JfxTextEditorCompone
                 throw new UnsupportedOperationException("Not able to show meter for " + item.getClass().getSimpleName());
             }
 
-            gc.setFill(asFxColor(getDrawingSettings().getColors().backgroundFor(status, ColorComponentType.TEXT_FIELD)));
+            gc.setFill(asFxColor(customDrawing.getColorFor(protectedCurrent, getDrawingSettings().getColors(), status,
+                    ColorComponentType.TEXT_FIELD).getBg()));
             gc.fillRect(0, 0, displayWidth, displayHeight);
 
             gc.setFill(Color.TRANSPARENT);
@@ -104,19 +105,17 @@ public class AnalogMeterComponent<T extends Number> extends JfxTextEditorCompone
                 for(var r : numCust.getColorRanges()) {
                     if (r.start() < protectedCurrent) {
                         gc.setStroke(asFxColor(r.bg()));
+                        double startOffs;
+                        var extent = ((Math.min(r.end(), protectedCurrent)-r.start()) / max) * -270;
                         if(getDrawingSettings().getJustification() == PortableAlignment.LEFT) {
-                            var startOffs = -180 + ((r.start() / max) * -270);
-                            var extent = ((Math.min(r.end(), protectedCurrent)-r.start()) / max) * -270;
-                            gc.strokeArc(8, 8, displayWidth - 16, displayHeight - 16, startOffs, extent, ArcType.OPEN);
+                            startOffs = -180 + ((r.start() / max) * -270);
                         } else {
-                            var startOffs = 270 + ((r.start() / max) * -270);
-                            var extent = ((Math.min(r.end(), protectedCurrent)-r.start()) / max) * -270;
-                            gc.strokeArc(8, 8, displayWidth - 16, displayHeight - 16,  startOffs, extent, ArcType.OPEN);
+                            startOffs = 270 + ((r.start() / max) * -270);
                         }
+                        gc.strokeArc(8, 8, displayWidth - 16, displayHeight - 16, startOffs, extent, ArcType.OPEN);
 
                     }
                 }
-
             } else {
                 gc.setStroke(asFxColor(getDrawingSettings().getColors().backgroundFor(lastStatus, ColorComponentType.HIGHLIGHT)));
                 double extent = Math.min(2, (protectedCurrent / max)) * -270;
