a36574c76f6fc17234f454f4bb29d5076840ec62
dave <dave@nutricherry.com>
2024-10-19 11:08:44 +0100

    Test cases all pass

--- tcMenuGenerator/src/test/java/com/thecoderscorner/menu/editorui/generator/ejava/EmbeddedJavaGeneratorTest.java
+++ tcMenuGenerator/src/test/java/com/thecoderscorner/menu/editorui/generator/ejava/EmbeddedJavaGeneratorTest.java
@@ -70,7 +70,7 @@ class EmbeddedJavaGeneratorTest {
                 .withProperties(plugin.getProperties()).codeOptions();
         List<CodePluginItem> plugins = List.of(plugin);
         generator.setLoggerFunction((level, s) -> Logger.getAnonymousLogger().log(Level.INFO, level + " " + s));
-        generator.startConversion(tempPath, plugins, tree, List.of("xyzoerj"), options, LocaleMappingHandler.NOOP_IMPLEMENTATION, List.of());
+        generator.startConversion(tempPath, plugins, tree, List.of("xyzoerj"), options, LocaleMappingHandler.NOOP_IMPLEMENTATION);
 
         var project = new EmbeddedJavaProject(tempPath, options, storage, LocaleMappingHandler.NOOP_IMPLEMENTATION, (level, s) -> Logger.getAnonymousLogger().log(Level.INFO, level + " " + s));
 
@@ -100,7 +100,7 @@ class EmbeddedJavaGeneratorTest {
                 .codeOptions();
         List<CodePluginItem> plugins = List.of(plugin);
         generator.setLoggerFunction((level, s) -> Logger.getAnonymousLogger().log(Level.INFO, level + " " + s));
-        generator.startConversion(tempPath, plugins, tree, List.of("xyzoerj"), options, LocaleMappingHandler.NOOP_IMPLEMENTATION, List.of());
+        generator.startConversion(tempPath, plugins, tree, List.of("xyzoerj"), options, LocaleMappingHandler.NOOP_IMPLEMENTATION);
 
         var project = new EmbeddedJavaProject(tempPath, options, storage, LocaleMappingHandler.NOOP_IMPLEMENTATION,
                 (level, s) -> Logger.getAnonymousLogger().log(Level.INFO, level + " " + s));
