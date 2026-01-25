46ee0cd3e0d6903c52cc6796a50673503f96da40
dave <dave@thecoderscorner.com>
2026-01-25 08:57:28 +0000

    First revision

--- tcMenuGenerator/src/main/java/com/thecoderscorner/menu/editorui/generator/core/CoreCodeGenerator.java
+++ tcMenuGenerator/src/main/java/com/thecoderscorner/menu/editorui/generator/core/CoreCodeGenerator.java
@@ -76,6 +76,7 @@ public abstract class CoreCodeGenerator implements CodeGenerator {
              """;
 
     protected final ArduinoLibraryInstaller installer;
+    protected final MenuTreeCodeGenerator menuCodeGenerator;
     protected final SketchFileAdjuster sketchAdjuster;
     protected final EmbeddedPlatform embeddedPlatform;
     protected final ConfigurationStorage configStore;
@@ -329,45 +330,6 @@ public abstract class CoreCodeGenerator implements CodeGenerator {
         return menuTree.getMenuItems(MenuTree.ROOT).stream().findFirst()
                 .map(menuItem -> "menu" + menuNameFor(menuItem))
                 .orElse("");
-    }
-
-    protected Collection<BuildStructInitializer> generateMenusInOrder(MenuTree menuTree) throws TcMenuConversionException {
-        List<MenuItem> root = menuTree.getMenuItems(MenuTree.ROOT);
-        List<List<BuildStructInitializer>> itemsInOrder = renderMenu(menuTree, root);
-        Collections.reverse(itemsInOrder);
-        return itemsInOrder.stream()
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-    }
-
-    protected List<List<BuildStructInitializer>> renderMenu(MenuTree menuTree, Collection<MenuItem> itemsColl) throws TcMenuConversionException {
-        ArrayList<MenuItem> items = new ArrayList<>(itemsColl);
-        List<List<BuildStructInitializer>> itemsInOrder = new ArrayList<>(100);
-        for (int i = 0; i < items.size(); i++) {
-
-            MenuItem item = items.get(i);
-            if (item.hasChildren()) {
-                int nextIdx = i + 1;
-                String nextSub = (nextIdx < items.size()) ? menuNameFor(items.get(nextIdx)) : MenuItemToEmbeddedGenerator.CPP_NULL_PTR;
-
-                List<MenuItem> childItems = menuTree.getMenuItems(item);
-                String nextChild = (!childItems.isEmpty()) ? menuNameFor(childItems.getFirst()) : MenuItemToEmbeddedGenerator.CPP_NULL_PTR;
-                itemsInOrder.add(MenuItemHelper.visitWithResult(item,
-                                new MenuItemToEmbeddedGenerator(menuNameFor(item), nextSub, nextChild,
-                                        false, localeHandler, menuTree))
-                        .orElse(Collections.emptyList()));
-                itemsInOrder.addAll(renderMenu(menuTree, childItems));
-            } else {
-                int nextIdx = i + 1;
-                Object defVal = MenuItemHelper.getValueFor(item, menuTree, MenuItemHelper.getDefaultFor(item));
-                String next = (nextIdx < items.size()) ? menuNameFor(items.get(nextIdx)) : MenuItemToEmbeddedGenerator.CPP_NULL_PTR;
-                itemsInOrder.add(MenuItemHelper.visitWithResult(item,
-                                new MenuItemToEmbeddedGenerator(menuNameFor(item), next, null,
-                                        toEmbeddedCppValue(item, defVal), localeHandler, menuTree))
-                        .orElse(Collections.emptyList()));
-            }
-        }
-        return itemsInOrder;
     }
 
     private String toEmbeddedCppValue(MenuItem item, Object defaultValue) throws TcMenuConversionException {
