3be4e6d0d04721cf2ed7f4e65eb1026863e032d1
dave <dave@nutricherry.com>
2026-02-01 09:24:14 +0000

    adding basic colors to portable color.

--- tcMenuJavaApi/src/main/java/com/thecoderscorner/menu/domain/state/PortableColor.java
+++ tcMenuJavaApi/src/main/java/com/thecoderscorner/menu/domain/state/PortableColor.java
@@ -9,6 +9,20 @@ import java.util.Objects;
 public class PortableColor {
     public static final PortableColor BLACK = new PortableColor(0, 0, 0);
     public static final PortableColor WHITE = new PortableColor(255, 255, 255);
+    public static final PortableColor RED = new PortableColor(255, 0, 0);
+    public static final PortableColor INDIGO = new PortableColor("#4B0082");
+    public static final PortableColor DARK_GREY = new PortableColor(80, 80, 80);
+    public static final  PortableColor GREY = new PortableColor(150, 150, 150);
+    public static final PortableColor LIGHT_GRAY = new PortableColor(200, 200, 200);
+    public static final PortableColor DARK_SLATE_BLUE = new PortableColor(72, 61, 139);
+    public static final  PortableColor ANTIQUE_WHITE = new PortableColor(250, 235, 215);
+    public static final  PortableColor DARK_BLUE = new PortableColor(0, 0, 139);
+    public static final PortableColor CRIMSON = new PortableColor(220, 20, 60);
+    public static final PortableColor CORAL = new PortableColor(0xff, 0x7f, 0x50);
+    public static final PortableColor CORNFLOWER_BLUE = new PortableColor(100, 149, 237);
+    public static final PortableColor BLUE = new PortableColor(0, 0, 255);
+    public static final PortableColor GREEN = new PortableColor(0, 255, 0);
+
     private final int red;
     private final int green;
     private final int blue;
